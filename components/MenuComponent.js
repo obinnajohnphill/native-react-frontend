c3464176c07d40a22c3929af8e33e56122d88612
Obinna Johnphill <obinna@livelinesuk.com>
2020-04-11 02:11:39 +0100

    getting lists of restaurants on api response

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,40 +7,61 @@ class MenuComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nameList: ["Chicken","Lamb","Rice"]
-        }
+            posts: [],
+            isLoading: true,
+            errors: null
+        };
     }
 
-
-    // make the GET request to fetch data from the URL then using promise function to handle response.
-    componentDidMount() {
+    // Now we're going to make a request for data using axios
+    getPosts() {
         const options = {
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
-                 'user-key': '769eead0dcfce8d55a40154cac886fde'
+                'user-key': '769eead0dcfce8d55a40154cac886fde'
             }
         };
         axios.get('https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318', options)
-            .then((response) => {
-                console.log("RESPONSE: ", response);
+            .then(response => {
+                const posts =  response.data;
+                this.setState({
+                    posts: [this.state.posts, posts],
+                    isLoading: false
+                });
             })
-            .catch((error) => {
-                console.log("ERROR: ", error);
-            })
+            // If we catch any errors connecting, let's update accordingly
+            .catch(error => this.setState({ error, isLoading: false }));
     }
-
-
+    // Let's our app know we're ready to render the data
+    componentDidMount() {
+        this.getPosts();
+    }
+    // Putting that data to use
     render() {
+        const { isLoading, posts} = this.state;
         return (
-            <ul>
-                {
-                    this.state.nameList.map(user => <li>{user}</li>)
-                }
-            </ul>
+            <React.Fragment>
+                <h2>List of Menu</h2>
+                <div>
+                    {!isLoading ? (
+                        posts.map(post => {
+                            const {id, name,cuisines} = post;
+                            return (
+                                <div key={id}>
+                                    <h2>{name}</h2>
+                                    <p>{cuisines}</p>
+                                    <hr />
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+                </div>
+            </React.Fragment>
         );
     }
 }
-
 
 // API Key: 769eead0dcfce8d55a40154cac886fde
 
