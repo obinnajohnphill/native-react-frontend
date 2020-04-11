6447bfa9d0cef706ce0e7b6396b27c47886c8830
Obinna Johnphill <obinna@livelinesuk.com>
2020-04-11 04:07:38 +0100

    getting restaurant on api response

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,49 +7,49 @@ class MenuComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: [],
+            restaurants: [],
             isLoading: true,
             errors: null
         };
     }
 
-    // Now we're going to make a request for data using axios
-    getPosts() {
+    getRestaurants() {
         const options = {
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
                 'user-key': '769eead0dcfce8d55a40154cac886fde'
             }
         };
-        axios.get('https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318', options)
+        axios.get('https://developers.zomato.com/api/v2.1/restaurant?res_id=19333744', options)
             .then(response => {
-                const posts =  response.data;
+                const restaurants = response.data;
                 this.setState({
-                    posts: [this.state.posts, posts],
+                    restaurants: [this.state.restaurants, restaurants],
                     isLoading: false
                 });
             })
             // If we catch any errors connecting, let's update accordingly
             .catch(error => this.setState({ error, isLoading: false }));
     }
-    // Let's our app know we're ready to render the data
+
+
     componentDidMount() {
-        this.getPosts();
+        this.getRestaurants()
     }
-    // Putting that data to use
+
+
     render() {
-        const { isLoading, posts} = this.state;
+        const { isLoading, restaurants} = this.state;
         return (
             <React.Fragment>
                 <h2>List of Menu</h2>
                 <div>
                     {!isLoading ? (
-                        posts.map(post => {
-                            const {id, name,cuisines} = post;
+                        restaurants.map(restaurant => {
                             return (
-                                <div key={id}>
-                                    <h2>{name}</h2>
-                                    <p>{cuisines}</p>
+                                <div key={restaurant.id}>
+                                    <h2>{restaurant.name}</h2>
+                                    <p>{restaurant.cuisines}</p>
                                     <hr />
                                 </div>
                             );
