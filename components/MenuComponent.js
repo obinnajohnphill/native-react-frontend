2f64803ecdc0ba70002b27841e442c6a3cf81492
Obinna Johnphill <obinna@livelinesuk.com>
2020-04-11 22:59:40 +0100

    menu response data sorted

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -14,6 +14,8 @@ class MenuComponent extends Component {
     }
 
     getRestaurants() {
+        let  array = [];
+        let v = [];
         const options = {
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
@@ -24,28 +26,30 @@ class MenuComponent extends Component {
             .then(response => {
                 let arr = [];
                 arr = Object.entries(response.data).filter(e => e !== 'daily_menus,');
-                console.log(arr[0][1])
-               // const daily_menus = arr[0][1];
-                this.setState({daily_menus: arr[0][1]});
-            })
-            // If we catch any errors connecting, let's update accordingly
-           // .catch(error => this.setState({ error, isLoading: false }));
+                const iterator = arr[0][1].values();
+                for (const value of iterator) {
+                    Object.entries(value).forEach(element => {
+                        array.push(element[1].dishes);
+                    });
+                }
+                this.setState({daily_menus: array[1]});
+            }).catch(error => {
+              console.log(error.response)
+        })
     }
-
 
     componentDidMount() {
         this.getRestaurants()
     }
 
     render() {
-       // console.log('data' + Object.entries(this.state))
         const {daily_menus} = this.state;
         const list = daily_menus.map((item, index) => {
             return (
                 <li className="list-container" key={index}>
                     <div className="text-container">
-                        <h4>Dish Name: {item.daily_menu.name}</h4>
-                        <h5>Start Date: {item.daily_menu.start_date}</h5>
+                        <h4>Dish Name: {item.dish.name}</h4>
+                        <h4>Price: {item.dish.price}</h4>
                         <button className="btn">Add +</button>
                         <hr/>
                     </div>
@@ -64,6 +68,5 @@ class MenuComponent extends Component {
 // API Key: 769eead0dcfce8d55a40154cac886fde
 
 // Get restaurant id: curl -X GET --header "Accept: application/json" --header "user-key: 769eead0dcfce8d55a40154cac886fde" "https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318"
-
 
 export default MenuComponent;
